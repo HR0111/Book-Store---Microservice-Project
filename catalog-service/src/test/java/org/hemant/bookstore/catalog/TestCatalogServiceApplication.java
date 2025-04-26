41c5c00caa82e78fa5826ec7437920d97deecee4
hr0111 <hemantrathod0111@gmail.com>
2025-04-26 08:03:00 +0530

    project setup done

---
+++ catalog-service/src/test/java/org/hemant/bookstore/catalog/TestCatalogServiceApplication.java
@@ -0,0 +1,26 @@
+package org.hemant.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+
+@TestConfiguration
+public class TestCatalogServiceApplication {
+
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(
+                DockerImageName.parse("postgres:16-alpine"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
